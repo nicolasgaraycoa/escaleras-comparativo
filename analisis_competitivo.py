c763259fdde8647c350ec51802132b388df030e1
nicolasgaraycoa <62861550+nicolasgaraycoa@users.noreply.github.com>
2023-08-08 13:53:58 -0400

    Add files via upload

---
+++ analisis_competitivo.py
@@ -0,0 +1,34 @@
+import os
+import pandas as pd
+import numpy as np
+import streamlit as st
+import plotly.express as px
+
+
+
+st.set_page_config(page_title="Analisis competitivo", layout="wide")
+
+st.title('Analisis competitivo')
+
+precios = pd.read_excel('C:/Users/nicol/Documents/Oliver/Oliver/analisis_competitivo/precios.xlsx')
+
+with st.sidebar:
+    pais = st.multiselect('Pais: ', list(precios.pais.unique()))
+    tipo = st.multiselect('Tipo de alcohol: ', list(precios.tipo.unique()))
+    tier = st.slider('Price tiers (n): ', min_value =2, max_value=5,
+                     value = 3, step=1)
+    
+
+precios = precios[precios['pais'].str.contains('|'.join(map(str, pais)))]
+precios = precios[precios['tipo'].str.contains('|'.join(map(str, tipo)))]
+bin_labels = ["A","B","C","D","E"][1:(tier-1)]
+precios['precio_bin'] = pd.qcut(precios['precio'], q=tier, 
+                                labels=["A","B","C","D","E"][:tier])
+
+
+fig = px.violin(precios, x="precio", y="precio_bin", color="sku", box=True, points=False,
+          hover_data=precios.columns)
+
+
+
+st.plotly_chart(fig, theme="streamlit", use_container_width=True)
