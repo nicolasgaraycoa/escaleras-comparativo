b464585e63a123b4a255b449588132b78f16b9ca
nicolasgaraycoa <62861550+nicolasgaraycoa@users.noreply.github.com>
2023-08-10 19:09:48 -0400

    Update analisis_competitivo.py

--- analisis_competitivo.py
+++ analisis_competitivo.py
@@ -11,7 +11,7 @@ st.set_page_config(page_title="Analisis competitivo", layout="wide")
 
 st.subheader('Analisis competitivo')
 
-precios = pd.read_excel('C:/Users/nicol/Documents/Oliver/Oliver/analisis_competitivo/precios.xlsx')
+precios = pd.read_excel('precios.xlsx')
 
 with st.sidebar:
     pais = st.multiselect('Pais: ', list(precios.pais.unique()))
