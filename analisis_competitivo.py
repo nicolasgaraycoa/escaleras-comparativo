be11bcaa45229d46d204566c30b9209c7f041cc1
nicolasgaraycoa <62861550+nicolasgaraycoa@users.noreply.github.com>
2023-08-10 19:05:37 -0400

    Update analisis_competitivo.py

--- analisis_competitivo.py
+++ analisis_competitivo.py
@@ -3,32 +3,49 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 import plotly.express as px
+import statistics
 
 
 
 st.set_page_config(page_title="Analisis competitivo", layout="wide")
 
-st.title('Analisis competitivo')
+st.subheader('Analisis competitivo')
 
-precios = pd.read_excel('precios.xlsx')
+precios = pd.read_excel('C:/Users/nicol/Documents/Oliver/Oliver/analisis_competitivo/precios.xlsx')
 
 with st.sidebar:
     pais = st.multiselect('Pais: ', list(precios.pais.unique()))
     tipo = st.multiselect('Tipo de alcohol: ', list(precios.tipo.unique()))
-    tier = st.slider('Price tiers (n): ', min_value =2, max_value=5,
-                     value = 3, step=1)
+    sku_ref = st.selectbox("SKU referencial: ", 
+                           list(precios['sku'].unique()))
+    marca = st.multiselect('Marcas: ', list(precios.marca.unique()))
     
 
 precios = precios[precios['pais'].str.contains('|'.join(map(str, pais)))]
 precios = precios[precios['tipo'].str.contains('|'.join(map(str, tipo)))]
-bin_labels = ["A","B","C","D","E"][1:(tier-1)]
-precios['ptier'] = pd.qcut(precios['precio'], q=tier, 
+
+comp = precios[['marca','sku', 'precio', 'ml']]
+comp['precio'] = (statistics.mode(comp['ml'])/comp['ml'])*comp['precio']
+comp.drop(columns=['ml'], inplace=True)
+psku_ref = precios['precio'][precios['sku']==sku_ref].mean()
+comp['pref'] = (comp['precio']/psku_ref)*100
+
+comp.drop(columns=['precio'], inplace=True)
+comp = comp.groupby(by=['marca','sku'], as_index=False).agg({'pref':['mean','std']})
+comp.columns = ['marca','sku', 'pref_mean', 'pref_std']
+comp['pref_mean'] = round(comp['pref_mean'],0)
+comp['pref_std'] = round(comp['pref_std'],0)
+comp = comp.fillna(value=1)
+
+comp = comp[comp['marca'].str.contains('|'.join(map(str, marca)))]
+tier = min(5, comp.shape[0])
+comp['ptier'] = pd.qcut(comp['pref_mean'], q=tier, 
                                 labels=["A","B","C","D","E"][:tier])
 
-
-fig = px.box(precios, y="precio", x="ptier", color="marca", hover_data=precios.columns)
-
+fig = px.scatter(comp, x="ptier", y="pref_mean", color="sku", opacity=0.6,
+                 title="Escaleras de precios x marca x sku")
+fig.update_traces(marker=dict(size=22))
+fig.add_hline(y=100, line_dash="dash", opacity=0.3)
 fig.update_xaxes(categoryorder='total ascending')
-
 
 st.plotly_chart(fig, theme="streamlit", use_container_width=True)
